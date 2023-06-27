49d6fe51d7bd41603721259ef156b957ce282914
Bahrul Rozak <bahrulrozak13062001@gmail.com>
2023-06-28 01:25:04 +0700

    Making supplier all page view and handle by SupplierController

--- app/Http/Controllers/Pos/SupplierController.php
+++ app/Http/Controllers/Pos/SupplierController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Pos;
 
 use App\Http\Controllers\Controller;
+use App\Models\Supplier;
 use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
     public function SupplierAll()
     {
+        // $suppliers = Supplier::all();
+        $suppliers = Supplier::latest()->get();
+        return view('backend.supplier.supplier_all', compact('suppliers'));
     }
 }
