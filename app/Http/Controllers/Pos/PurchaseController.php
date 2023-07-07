e5dcdf737ada93fd758b4b3f2384d76f16dfd44e
Bahrul Rozak <bahrulrozak13062001@gmail.com>
2023-07-07 23:12:24 +0700

    Feat (Purchase Page) : Create resource views for purchase all

--- app/Http/Controllers/Pos/PurchaseController.php
+++ app/Http/Controllers/Pos/PurchaseController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Pos;
 
-use App\Http\Controllers\Controller;
+use App\Models\Purchase;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PurchaseController extends Controller
 {
-    //
+    public function PurchaseAll()
+    {
+
+        $allData = Purchase::orderBy('date', 'desc')->orderBy('id', 'desc');
+        return view('backend.purchase.purchase_all', compact('allData'));
+    }
 }
