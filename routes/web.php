06abb3fdd20774fbb56db1950054fad657049d35
Bahrul Rozak <bahrulrozak13062001@gmail.com>
2023-07-07 21:19:59 +0700

    Fix (Product page) and Feat : Create Method for Edit and Update

--- routes/web.php
+++ routes/web.php
@@ -68,6 +68,8 @@ Route::controller(ProductController::class)->group(function () {
     Route::get('/product/all', 'ProductAll')->name('product.all');
     Route::get('/product/add', 'ProductAdd')->name('product.add');
     Route::post('/product/store', 'ProductStore')->name('product.store');
+    Route::get('/product/edit/{id}', 'ProductEdit')->name('product.edit');
+    Route::post('/product/update', 'ProductUpdate')->name('product.update');
 });
 
 Route::get('/dashboard', function () {
